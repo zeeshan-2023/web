2b612ceeae0c21cdbac922df441b446c2c20d18d
zeeshan <142723633+zeeshan-2023@users.noreply.github.com>
2023-09-26 09:02:09 +0530

    Update models.py

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    unmane=models.CharField(max_length=50)
+    upassword=models.CharField(max_length=50)
+    uemail=models.CharField(max_length=50)
+
+class Meta:
+    db_table='users'
